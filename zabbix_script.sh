870743d841b3b2fbee9b3374a6f9a821f1471e21
joaochiroli <joaochiroli123@gmail.com>
2024-08-10 14:33:51 -0400

    commit

---
+++ zabbix_script.sh
@@ -0,0 +1,86 @@
+#!/bin/bash
+
+
+echo "Quantos hosts serão adicionados?"
+read numero_hosts
+
+# Loop enquanto 'numero_hosts' for maior que 0
+while [ $numero_hosts -gt 0 ]
+do
+    echo "Qual o hostname do servidor?"
+    read hostname
+
+    echo "Qual o IP do servidor?"
+    read ip
+
+    # Chamada ao API com curl
+    curl --silent --location 'http://192.168.15.12/api_jsonrpc.php' \
+    --header 'Content-Type: application/json' \
+    --data '{
+        "jsonrpc": "2.0",
+        "method": "hostgroup.get",
+        "params": {
+            "output": ["name","groupid"]
+        },
+        "auth": "4c67e35e1f544115bfbe1b4908ee2b96",
+        "id": 1
+    }' | jq -r '.result[] | "[groupid: \"\(.groupid)\", name: \"\(.name)\"]"'
+
+    echo "Qual o grupo esse servidor deve fazer parte? Coloque o número do groupid escolhido, exemplo: 6"
+    read grupo
+
+    # Chamada ao API com curl
+    curl --silent --location 'http://192.168.15.12/api_jsonrpc.php' \
+    --header 'Content-Type: application/json' \
+    --data '{
+        "jsonrpc": "2.0",
+        "method": "template.get",
+        "params": {
+            "output": ["name","templateid"]
+    },
+    "auth": "4c67e35e1f544115bfbe1b4908ee2b96",
+    "id": 1
+    }' | jq -r '.result[] | "[templateid: \"\(.templateid)\", name: \"\(.name)\"]"'
+
+    echo "Qual o template esse servidor deve ter? Coloque o número do templateid escolhido, exemplo: 10001"
+    read template
+
+    curl --silent --location 'http://192.168.15.12/api_jsonrpc.php' \
+    --header 'Content-Type: application/json' \
+    --data "{
+    \"jsonrpc\": \"2.0\",
+    \"method\": \"host.create\",
+    \"params\": {
+        \"host\": \"$hostname\",
+        \"groups\": [
+            {
+                \"groupid\": \"$grupo\"
+            }
+        ],
+        \"interfaces\": [
+            {
+                \"type\": \"1\",
+                \"main\": \"1\",
+                \"useip\": \"1\",
+                \"ip\": \"$ip\",
+                \"dns\": \"\",
+                \"port\": \"10050\"
+            }
+        ],
+        \"templates\": [
+            {
+                \"templateid\": \"$template\"
+            }
+        ]
+    },
+    \"auth\": \"4c67e35e1f544115bfbe1b4908ee2b96\",
+    \"id\": 1
+    }" > /dev/null 2>&1
+
+    echo -e "\nO host '$hostname' acabou de ser criado\n"
+
+    # Decrementa o número de hosts restantes
+    numero_hosts=$((numero_hosts - 1))
+done
+
+echo "Todos os hosts foram adicionados. Número de hosts restantes: $numero_hosts"
